653327978eb83aa081df67ff12f2caa6c4803e4c
owolfdev <owolfdev@gmail.com>
2023-12-27 05:56:52 +0700

    replace middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,27 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+const botUserAgents = [
+  "Google Page Speed Insights", // Example entry, replace with actual User-Agent strings
+];
+const customMiddleware = (request: any) => {
+  const userAgent = request.headers.get("user-agent") || "";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: ["/", "/contact"],
-});
+  // Check if the request is from a known bot
+  const isBot = botUserAgents.some((botUserAgent) =>
+    userAgent.includes(botUserAgent)
+  );
+
+  if (isBot) {
+    // Bypass auth for bots
+    return NextResponse.next();
+  }
+
+  authMiddleware({
+    publicRoutes: ["/", "/contact"],
+  });
+};
+
+export default customMiddleware;
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
