ca167381cc10ea840bf7615c783975adbb82525c
owolfdev <owolfdev@gmail.com>
2023-12-27 06:07:25 +0700

    replace middleware

---
+++ app/clerk/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserButtonComponent from "./user-button";
+
+function Test() {
+  return (
+    <>
+      <div>
+        <UserButtonComponent />
+      </div>
+    </>
+  );
+}
+
+export default Test;
--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
-const botUserAgents = [
-  "Google Page Speed Insights", // Example entry, replace with actual User-Agent strings
-];
-const customMiddleware = (request: any) => {
-  const userAgent = request.headers.get("user-agent") || "";
-
-  // Check if the request is from a known bot
-  const isBot = botUserAgents.some((botUserAgent) =>
-    userAgent.includes(botUserAgent)
-  );
-
-  if (isBot) {
-    // Bypass auth for bots
-    return NextResponse.next();
-  }
-
-  authMiddleware({
-    publicRoutes: ["/", "/contact"],
-  });
-};
-
-export default customMiddleware;
+export default authMiddleware({
+  // "/" will be accessible to all users
+  publicRoutes: ["/"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
